934213affab6856d92a1d6b85c2e77c27b59ddd9
ggosdinski <gianfranco.gosdinski@icloud.com>
2025-03-21 18:16:13 -0500

    Dashboard

--- react-todo-list/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders To-Do List title', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/To-Do List/i);
-  expect(titleElement).toBeInTheDocument();
-});
